0a90a70f5f6e50a7a9ffa96d9e4db1eca18290e4
Song, Haifeng <haifeng.song@hpe.com>
2017-06-30 17:12:59 +0800

    Support 3.10 storage system

--- examples/addStorageSystem.js
+++ examples/addStorageSystem.js
@@ -11,7 +11,7 @@ module.exports = function addStorageSystem(ip) {
     yield client.login();
 
     const existingStorageSystemList = yield client.get({
-      uri: '/rest/storage-systems',
+      uri: '/rest/storage-systems?count=40',
     });
 
     const storageSystemAddresses = config['storage-systems'];
@@ -20,74 +20,65 @@ module.exports = function addStorageSystem(ip) {
     for(let i = 0; i < storageSystemAddresses.length; i += 1) {
       const storageSystemAddress = storageSystemAddresses[i];
       const storageSystemArray = existingStorageSystemList.members.filter((storageSystem) => {
-        return storageSystem.credentials.ip_hostname === storageSystemAddress;
+        return storageSystem.hostname === storageSystemAddress;
       });
       if (storageSystemArray.length === 0 ) {
         const postRes = yield client.post({
           uri: '/rest/storage-systems',
           resolveWithFullResponse: true,
           body: {
-            'ip_hostname': storageSystemAddress,
+            'hostname': storageSystemAddress,
             'username': 'dcs',
             'password': 'dcs',
+            'family' : 'StoreServ',
           },
         });
-        console.log(`[${ip}] is posted, task: ${postRes.headers.location}`);
+        console.log(`[${ip}] ${storageSystemAddress} is posted, task: ${postRes.headers.location}`);
         storageSystem = yield client.waitTaskComplete(postRes.headers.location);
         console.log(`[${ip}] ${storageSystemAddress} is ${storageSystem.state}`);
       } else if (storageSystemArray.length === 1) {
         storageSystem = storageSystemArray[0];
       } else {
         console.error(`[${ip}] two same storage system with same address ${JSON.stringify(storageSystemArray)}`);
+        return;
+      }
+
+      if (storageSystem.deviceSpecificAttributes.managedDomain === config.storageSystemDomain) {
+        continue;
       }
 
       // Add storage-pools into storage-system
-      const storagePools = [].concat(storageSystem.unmanagedPools, storageSystem.managedPools)
-        .filter(storagePool => storagePool.domain === config.storageSystemDomain)
-        .map((storagePool) => {
-          return {
-            deviceType: storagePool.deviceType,
-            domain: storagePool.domain,
-            name: storagePool.name,
-            type: 'StoragePool',
-          };
-        });
-      const unmanagedPorts = storageSystem.unmanagedPorts.map((unmanagedPort) => {
-        return {
-          actualNetworkSanUri: unmanagedPort.actualNetworkSanUri,
-          actualNetworkUri: unmanagedPort.actualNetworkUri,
-          expectedNetworkUri: unmanagedPort.expectedNetworkUri,
-          groupName: unmanagedPort.groupName,
-          label: unmanagedPort.label,
-          name: unmanagedPort.name,
-          portName: unmanagedPort.portName,
-          portWwn: unmanagedPort.portWwn,
-          protocolType: unmanagedPort.protocolType,
-          type: unmanagedPort.type,
-        };
-      });
+      delete storageSystem.displayName;
+      delete storageSystem.totalCapacity;
+      delete storageSystem.allocatedCapacity;
+      delete storageSystem.freeCapacity;
+      delete storageSystem.state;
+      delete storageSystem.status;
+      delete storageSystem.modified;
+      delete storageSystem.lastRefreshTime;
+      storageSystem.deviceSpecificAttributes.discoveredDomains = storageSystem.deviceSpecificAttributes.discoveredDomains.filter(
+        domain => domain !== config.storageSystemDomain
+      );
+      storageSystem.deviceSpecificAttributes.managedDomain = config.storageSystemDomain;
+      storageSystem.deviceSpecificAttributes.managedPools = storageSystem.deviceSpecificAttributes.discoveredPools.filter(
+        pool => pool.domain === config.storageSystemDomain
+      );
+      // don't assign discoveredPools before assign managedPools
+      storageSystem.deviceSpecificAttributes.discoveredPools = storageSystem.deviceSpecificAttributes.discoveredPools.filter(
+        pool => pool.domain !== config.storageSystemDomain
+      );
+
       const putRes = yield client.put({
         uri: storageSystem.uri,
         resolveWithFullResponse: true,
         headers: {
           'If-Match': storageSystem.eTag,
         },
-        body: {
-          credentials: storageSystem.credentials,
-          eTag: storageSystem.eTag,
-          managedDomain: config.storageSystemDomain,
-          managedPools: storagePools,
-          managedPorts: [],
-          name: storageSystem.name,
-          serialNumber: storageSystem.serialNumber,
-          type: storageSystem.type,
-          unmanagedPorts,
-          uri: storageSystem.uri,
-        },
+        body: storageSystem,
       });
       console.log(`[${ip}] storage-pools are put into storage-system, task: ${putRes.headers.location}`);
       storageSystem = yield client.waitTaskComplete(putRes.headers.location);
-      console.log(`[${ip}] ${storageSystem.managedPools.length} storage-pools are in storageSystemUri`);
+      console.log(`[${ip}] storage-pools are in storageSystemUri`);
     }
   });
 };
@@ -96,6 +87,10 @@ if (require.main === module) {
   module.exports(process.argv[2]).then(() => {
     console.log('Done');
   }).catch((err) => {
-    console.error(`[${ip}] ${err.name} ${err.message}, stack:${err.stack}`);
+    if (err instanceof Error) {
+      console.error(`[${process.argv[2]}] ${err.name} ${err.message}, stack:${err.stack}`);
+    } else {
+      console.error(`[${process.argv[2]}] ${JSON.stringify(err)}`);
+    }
   });
 }
